39e4b6c49c60ba369dc0e16af1588df1d8455002
Sam Walker <sjwalker189@gmail.com>
2025-01-23 13:02:14 +1300

    Syntax highligh wip

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,1 +1,30 @@
-(source_file) @string
+[
+ "enum"
+ "model"
+ "view"
+] @keyword
+
+(comment) @comment
+; (developer_comment) @comment
+
+(arguments) @property
+; (attribute) @function
+(call_expression) @function
+(column_type) @type
+(enumeral) @constant
+(identifier) @variable
+(string) @string
+
+(member_expression) @property
+(property_identifier) @property
+(type_identifier) @type
+
+"(" @punctuation.bracket
+")" @punctuation.bracket
+"[" @punctuation.bracket
+"]" @punctuation.bracket
+"{" @punctuation.bracket
+"}" @punctuation.bracket
+"=" @operator
+; "@" @operator
+
