8629d571021927b8b2ccb1f62f3a226449d2caa4
Sam Walker <sjwalker189@gmail.com>
2025-01-24 14:26:35 +1300

    Rework using go grammar as base

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,30 +1,11 @@
-[
- "enum"
- "model"
- "view"
-] @keyword
-
 (comment) @comment
-; (developer_comment) @comment
+(developer_comment) @comment
 
-(arguments) @property
-; (attribute) @function
-(call_expression) @function
-(column_type) @type
-(enumeral) @constant
-(identifier) @variable
-(string) @string
+; "(" @punctuation.bracket
+; ")" @punctuation.bracket
+; "[" @punctuation.bracket
+; "]" @punctuation.bracket
+; "{" @punctuation.bracket
+; "}" @punctuation.bracket
 
-(member_expression) @property
-(property_identifier) @property
-(type_identifier) @type
-
-"(" @punctuation.bracket
-")" @punctuation.bracket
-"[" @punctuation.bracket
-"]" @punctuation.bracket
-"{" @punctuation.bracket
-"}" @punctuation.bracket
-"=" @operator
-; "@" @operator
 
---
+++ queries/tags.scm
@@ -0,0 +1,10 @@
+; ADT definitions
+
+; (model_declaration
+;     name: (type_identifier) @name) @definition.class
+;
+; (enum_declaration
+;     name: (type_identifier) @name) @definition.class
+;
+; (enum_body
+;     (field_identifier) @name) @constant
